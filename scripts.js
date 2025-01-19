25c9f3e00cdf5a8bb433081be3c1cdc53cf8e8b0
chloe-ui218 <chloerhinan10@gmail.com>
2025-01-18 22:25:25 -0800

    chore: adding the data set

--- scripts.js
+++ scripts.js
@@ -1,15 +1,35 @@
-const loginForm = document.querySelector('#login-form')
+const users = [
+    {
+        username: 'admin',
+        password: 'admin123',
+        role: 'admin'
+    },
+    {
+    username: 'editor',
+    password: 'editor123',
+    role: 'editor'
+    },
+    {
+        username: 'viewer',
+        password: 'viewer123',
+        role: 'viewer'
+    }
+]
+
+const loginForm = document.querySelector('#login-form');
 
 loginForm.addEventListener('submit', function(e) {
     e.preventDefault();
     
-    const username = document.querySelector('#userName').value;
-    const password = document.querySelector('#password').value;
-    const errorContainer = document.querySelector('#errorMessage');
-    // errorContainer.innerHTML = '';
+    const username = document.querySelector('#username').value.trim();
+    const password = document.querySelector('#password').value.trim();
+    const errorContainer = document.querySelector('#error-message');
 
-    if (!username.trim() || !password.trim()) {
-        errorContainer.textContent = 'Please fill in all fields';
-    } else {
-    } errorContainer.textContent = '';
-})
+    const user = users.find(function(user) {
+
+    // if (!username || !password) {
+    //     errorContainer.textContent = 'Please fill in all fields';
+    // } else {
+    //     errorContainer.textContent = '';
+    //}
+});
